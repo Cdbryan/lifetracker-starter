f6a1ccfc7f10fc6dcfc96192a3ad79028f1b6eb1
Christian Bryan <christian.bryan@christi-ltm3ydr.internal.salesforce.com>
2022-07-12 16:57:27 -0700

    create user logic working

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,118 @@
 const express = require('express')
 const router = express.Router()
+const {createUserJwt} = require("../utils/tokens")
+const security = require("../middleware/security")
+const User = require("../models/user");
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
-// define the home page route
-router.get('/', (req, res) => {
-  res.send('Birds home page')
-})
-// define the about route
-router.get('/about', (req, res) => {
-  res.send('About birds')
-})
+// // define the home page route
+// router.get('/', (req, res) => {
+//   res.send('Birds home page')
+// })
+// // define the about route
+// router.get('/about', (req, res) => {
+//   res.send('About birds')
+// })
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.login(req.body);
+    const token = createUserJwt(user);
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await User.register({ ...req.body, isAdmin: false });
+    const token = createUserJwt(user);
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/me", security.requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    const { email } = res.locals.user;
+    const user = await User.fetchUserByEmail(email);
+    const publicUser = User.makePublicUser(user);
+    return res.status(200).json({ user: publicUser });
+  }
+  catch (err){
+    next(err);
+  }
+});
+
+
+// router
+//     .route("/login")
+//     .post((req, res, next) => {
+//         const userObj = req.body;
+
+//         if (!userObj.email || !userObj.pw) {
+//             res.status(400).send("missing required fields");
+//         } else {
+//             const userFromDB = getUser(userObj.email);
+
+//             if (!userFromDB) {
+//                 res.status(400).send("no user account found");
+//             } else {
+//                 if (bcrypt.compareSync(userObj.pw, userFromDB.pw)) {
+//                     res.status(200).send("login success");
+//                 } else{
+//                     res.status(400).send("bad pw");
+//                 }
+//             }
+//         }
+//     });
+
+// router
+//     .route("/register")
+//     .post((req, res, next) => {
+//         try {
+//             const userObj = req.body;
+
+//             if (!userObj.email || !userObj.pw) {
+//                 res.status(400).send("missing required fields");
+//             } else {
+//                 const userFromDB = getUser(userObj.email);
+//                 if(!userFromDB) {
+//                     const hashedPw = bcrypt.hashSync(
+//                         userObj.pw,
+//                         10
+//                     );
+//                     const savedUser = saveUser(userObj.email. hashedPw);
+//                     if(savedUser) {
+//                         res.status(201).send(savedUser);
+//                     }
+//                 } else {
+//                     res.status(400).send("user already exists");
+//                 }
+//             }
+//         } catch (err) {
+//             res.status(500).render("error.html")
+//         }
+//     })
+
+// exports.getUser = async (email) => {
+//     const res = await client
+//         .query('select * from "user" where email=$1', [email]);
+//     return res.rows[0];
+// }
+// exports.saveUser = async (newUser, hashedPw) => {
+//     const res = await client
+//         .query(
+//             'insert into "user" values ($1,$2)',
+//             [newUser.email, hashedPw]
+//         )
+//     return res.rows[0];
+// }
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,29 @@ const morgan = require("morgan")
 const app = express()
 const port = 3001
 const authRouter = require("./routes/auth")
+const security = require("./middleware/security")
+const {NotFoundError} = require("./utils/errors")
+
 
 app.use(cors())
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use(security.extractUserFromJwt)
+
 app.use("/auth" , authRouter)
+
+app.use((req, res, next) => {
+  return next(new NotFoundError())
+})
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message
+
+  return res.status(status).json({
+    error: { message, status },
+  })
+})
 
 app.get('/', (req, res) => {
   res.send("Hello World!")
